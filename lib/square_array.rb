c88f05cd43b8b41511d61272d08396ea756cdf55
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-25 15:34:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,5 @@
-array = [9,10,16,25]
+numbers = [9,10,16,25]
 
-def square_array(array)
-  new_array = []
-  counter = 0
-  while counter < array.length()
-  
-   newsquare = 
- 
-  # your code here
-end
 def square_array(numbers)
   new_array = []
   counter = 0 
